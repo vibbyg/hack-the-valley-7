50c875ecd20df9ce0df3800e6174a98cb3908840
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-10-15 03:23:57 -0400

    refactor(nav)

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,22 +1,26 @@
+<script lang="ts">
+	let currency = 420;
+	let title = 'Tit';
+</script>
+
 <nav>
     <div id="spacer1"></div>
     <div id="currency">{currency}</div>
     <div id="title">{title}</div>
     <div id="spacer2"></div>
-    <div id="gacha">Gacha</div>
 </nav>
 
 <style>
-    nav {
+    nav{
         display: flex;
-        background-color: red;
+        background-color: var(--theme);
     }
 
-    #spacer1 {
+    #spacer1{
         flex: 1;
     }
 
-    #currency {
+    #currency{
         background-color: blue;
         text-align: center;
         border-radius: 10px;
@@ -25,52 +29,14 @@
         flex: 2;
     }
 
-    #title {
+    #title{
         text-align: center;
         margin: 10px 100px;
         padding: 10px;
         flex: 7;
     }
 
-    #spacer2 {
+    #spacer2{
         flex: 3;
     }
-
-    #gacha {
-        background-color: purple;
-        text-align: center;
-        position: absolute;
-        width: 200px;
-        height: 200px;
-        line-height: 200px;
-        border-radius: 100%;
-        top: 20px;
-        right: 20px;
-    }
-
-    /* nav > ul {
-        flex: 5;
-        display: flex;
-        list-style-type: none;
-        margin: 0 0 25px;
-        padding: 0;
-    }
-
-    nav > ul > li {
-        text-align: center;
-        font-size: large;
-    } */
-
-    /* nav > ul a {
-        color: white;
-        text-decoration: none;
-        display: block;
-        padding: 10px;
-        transition: background-color 0.5s, color 0.5s;
-    } */
 </style>
-
-<script>
-    let currency = 420;
-    let title = 'Tit';
-</script>
