34768b44e491107e8640b22f423f1fc345af741d
Eric Wan <ericwan957@gmail.com>
2022-10-15 18:09:53 -0400

    Create modal box

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    export let title: string;
+    export let isOpen = true;
+
+    function close(): void {
+        isOpen = false;
+    }
+</script>
+
+{#if isOpen}
+<div id="blocker">
+    <div id="box">
+        <div id="title">{title}</div>
+        <div id="body">
+            <slot></slot>
+        </div>
+        <div id="close" class="material-icons-round" on:click="{close}">
+            close
+        </div>
+    </div>
+</div>
+{/if}
+
+<style>
+    @import "https://fonts.googleapis.com/icon?family=Material+Icons+Round";
+
+    #blocker {
+        background-color: #0000007f;
+        z-index: 999;
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #box {
+        background-color: white;
+        border-radius: 10px;
+        position: absolute;
+        width: 20em;
+    }
+
+    #title {
+        text-align: center;
+        font-weight: bold;
+        font-size: 24pt;
+        padding: 0.5em;
+    }
+
+    #body {
+        padding: 0.5em;
+    }
+
+    #close {
+        font-size: 1.2em;
+        user-select: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 0.2em;
+        cursor: pointer;
+    }
+</style>
