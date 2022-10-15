ddd955120d5ed5e66d5f8e8aa006b29901a366af
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-10-15 18:14:33 -0400

    fix: default page is collections

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -7,7 +7,7 @@
     import { storage } from './stores';
     import NavHori from './NavHori.svelte';
 
-    let selected = 'Calculator';
+    let selected = 'Collection';
     const pages = { Gacha, Collection, Calculator, Info };
 
 </script>
