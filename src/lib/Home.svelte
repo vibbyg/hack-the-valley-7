104ae6d62d3b874318793f7669f585b0222557d3
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-10-15 05:32:01 -0400

    feat: gacha fits theme

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,39 +1,28 @@
 <script lang="ts">
-	import Gacha from './Gacha.svelte';
-	import Collected from './Collected.svelte';
-	import Recipe from './Recipe.svelte';
-	import Navbar from './Navbar.svelte';
+
 	import { storage } from './stores';
+	import Recipe from './Recipe.svelte';
+	import Collected from './Collected.svelte';
 </script>
 
-<main class="{$storage.option}">
+<main class="{$$props.class}">
     <section id="left">
-        <Navbar/>
-        <div id="recipes">
-            {#each $storage.recipes as recipe}
-                <Recipe {recipe}/>
-            {/each}
-        </div>
+        {#each $storage.recipes as recipe}
+            <Recipe {recipe}/>
+        {/each}
     </section>
     <section id="right">
-        <Gacha/>
         <Collected/>
     </section>
 </main>
+
 <style lang="sass">
     main
         display: flex
-        height: 100vh
-    section
-        height: 100%
-        display: flex
-        border: 1px solid var(--theme)
-        flex-direction: column
     #right
         flex: 1
     #left
         flex: 3
-    #recipes
         display: grid
         gap: 1em
         grid-template-columns: 1fr 1fr 1fr
---
+++ src/lib/Main.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Gacha from './Gacha.svelte';
+	import Home from './Home.svelte';
+	import Navbar from './Navbar.svelte';
+	import { storage } from './stores';
+
+	let selected = 'Home';
+	const pages = {
+		Gacha, Home
+	};
+</script>
+<Navbar bind:selected/>
+<svelte:head><title>{selected}</title></svelte:head>
+<svelte:component this="{pages[selected]}" class="{$storage.option}"/>
